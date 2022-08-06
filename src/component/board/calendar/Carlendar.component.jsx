6dce2b40d26fe2c84911740b858dce62c87b67ba
Trelawney <00uj@naver.com>
2022-08-06 12:57:48 +0900

    feature: get data by changing selected month

--- src/component/board/calendar/Carlendar.component.jsx
+++ src/component/board/calendar/Carlendar.component.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StyledCalendar, CalendarContainer } from './Calendar.style';
 import 'react-calendar/dist/Calendar.css';
 import dayjs from 'dayjs';
+import { getDietPerMonth } from '../../../utils/firebase/firebase';
+
+const dietHandler = async(year, month) => {
+    const data = await getDietPerMonth(year, month);
+    console.log(year, month, data);
+    return data
+}
 
 const Calendar = () => {
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [year, setYear] = useState(new Date().getFullYear());
   const navigate = useNavigate();
   const clickHandler = (day) => {
     const convertedate = 
@@ -14,12 +24,25 @@ const Calendar = () => {
     navigate(`/meal/${convertedate}`)
   }
 
+  const monthYearChangeHandler = ({action, activeStartDate}) => {
+    const date = new Date(activeStartDate);
+    setMonth(date.getMonth());
+    setYear(date.getFullYear())
+  }
+
+  useEffect(()=>{
+    const getDietCurrentMonth = async() => await dietHandler(year, month);
+    getDietCurrentMonth()
+  },[month, year]);
 
   return (
     <CalendarContainer>
       <StyledCalendar 
         formatDay ={(locale, date) => dayjs(date).format('DD')}
         onClickDay={(day)=>clickHandler(day)}
+        prev2Label={null}
+        next2Label={null}
+        onActiveStartDateChange={monthYearChangeHandler}
       />
     </CalendarContainer>
   )
--- src/page/Board/Board.component.jsx
+++ src/page/Board/Board.component.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Calendar from "../../component/board/calendar/Carlendar.component";
 import UserInfo from "../../component/board/userInfo/UserInfo.component";
 import CountWater from "../../component/board/countWater/CountWater.component";
@@ -6,18 +6,11 @@ import Diet from "../../component/board/dietTable/Diet.component";
 
 import { Switch } from 'antd';
 import { Section, LeftContainer, RightContainer, BoardContainer } from "./Board.style";
-import { getDietPerMonth } from "../../utils/firebase/firebase";
+
 
 const Board = () => {
   const [toggle, setToggle] = useState(true);
   const toggleHandler = () => setToggle((prev)=>!prev);
-
-  const handler = async() => {
-    const year = new Date().getFullYear();
-      const month = new Date().getMonth();
-      const data = await getDietPerMonth(year, month);
-      console.log(data);
-  }
 
   return(
     <Section>
@@ -31,7 +24,6 @@ const Board = () => {
           {toggle ? <Diet/> : <Calendar />}
         </RightContainer>
       </BoardContainer>
-      <button onClick={handler}>데이터 받아오기</button>
     </Section>
   )
 }
